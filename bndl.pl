89ddcdd5da871a8a47a17d949d803abf6d08148a
Joe Papperello <joe@socialflow.com>
2013-06-30 18:39:28 +0400

    Bootstrap files handle bindle-specific weirdness, so that we can softlink to the actual files where possible

--- bndl.pl
+++ bndl.pl
@@ -7,10 +7,10 @@ my ( $host, $port ) = @ARGV;
 $port ||= 22;
 $host ||= 'localhost';
 my $took = time();
-print "Getting $host ready for our arrival... \n";
+print "Getting $host ready for our arrival... ";
 my $home = $ENV{HOME};
 my $pup =
-  App::Bindle->new( host => $host, ssh_options => [ '-A', "-p $port" ], local_dir => "$home/.puptent/" );
+  App::Bindle->new( host => $host, ssh_options => [ '-A', "-p $port" ], local_dir => "$home/.bindle/" );
 $pup->copy_to_remote_recursive;
 print "All set!\n";
 $took = time() - $took;
@@ -21,6 +21,6 @@ if ( my $pid = fork() ) {
 }
 else {
     my $cmd = sprintf( qq|ssh -A -p %s -t %s "/bin/bash --rcfile %s -i"|,
-        $port, $pup->host, $pup->copied_files->{'bashrc.pup'} );
+        $port, $pup->host, $pup->copied_files->{'bootstrap_bashrc.pup'} );
     exec($cmd);
 }
